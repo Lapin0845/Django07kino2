6f0e3dc2d362378f10883e650a4f18df6126306e
Lapin0845 <shady08@list>
2023-09-20 23:09:57 +0400

    make models

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.maclass Genre(models.Model):
+# Create your models here.
+class Genre(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
