e269856d331c043028483f0cddb9cacabfd5f142
Lapin0845 <shady08@list>
2023-09-27 20:44:29 +0400

    Vetclinica

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Genre)
+admin.site.register(Focus)
 #admin.site.register(Director)
 #admin.site.register(Actor)
-admin.site.register(AgeRate)
+admin.site.register(Status)
 #admin.site.register(Status)
 #admin.site.register(Kino)
 admin.site.register(Country)
