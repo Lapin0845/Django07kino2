b1bf7198fa2cf9b8f5cf269550c7f2df44becff7
Lapin0845 <shady08@list>
2023-09-27 16:42:40 +0400

    Vetclinica

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Genre(models.Model):
-    name = models.CharField(max_length=20, verbose_name='Жанр')
+class Focus(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Направленность')
 
     def __str__(self):
         return self.name
@@ -14,45 +14,42 @@ class Director(models.Model):
     def __str__(self):
         return f'{self.fname}, {self.lname}'
 
-class Actor(models.Model):
+class Doctor(models.Model):
     fname = models.CharField(max_length=20, verbose_name='Имя')
     lname = models.CharField(max_length=20, verbose_name='Фамилия')
-    born = models.DateField()
-    country = models.CharField(max_length=20)
 
     def __str__(self):
         return self.lname
 
 class Status(models.Model):
-    vibor = (('бесплатно', 'бесплатно'),('базовая','базовая'),('супер','супер'))
-    name = models.CharField(max_length=20, choices=vibor, verbose_name='Подписка')
+    vibor = (('Профилактика', 'Профилактика'),('Лечение'))
+    name = models.CharField(max_length=20, choices=vibor, verbose_name='Заболевание')
 
     def __str__(self):
         return self.name
 
-class Country(models.Model):
-    name = models.CharField(max_length=20, verbose_name='Страна')
+class Street(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Улица')
 
     def __str__(self):
         return self.name
 
-class AgeRate(models.Model):
-    choise = (('G', 'G'), ('PG', 'PG'), ('PG-13', 'PG-13'), ('R', 'R'), ('NC-17', 'NC-17'))
-    rate = models.CharField(max_length=20, choices=choise, verbose_name='Рейтинг')
+class Reception(models.Model):
+    choise = (('Первичный 1000р', 'Первичный 1000р'), ('Второй 700р', 'Второй 700р'))
+    rate = models.CharField(max_length=20, choices=choise, verbose_name='Прием')
 
     def __str__(self):
         return self.rate
 
-class Kino(models.Model):
-    title = models.CharField(max_length=20, verbose_name='Фамилия')
-    ganre = models.ForeignKey(Genre, on_delete=models.SET_DEFAULT, default=1)
+class Vetclinika(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Ласка')
+    focus = models.ForeignKey(Focus, on_delete=models.SET_DEFAULT, default=1)
     rating = models.FloatField(verbose_name='Оценка')
-    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    street = models.ForeignKey(Street, on_delete=models.SET_NULL, null=True)
     director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=500, verbose_name='Описание')
-    year = models.IntegerField(verbose_name='Год')
-    ager = models.ForeignKey(AgeRate, on_delete=models.SET_NULL, null=True)
-    actor = models.ManyToManyField(Actor, verbose_name='Актеры')
+    recep = models.ForeignKey(Reception, on_delete=models.SET_NULL, null=True)
+    doct = models.ManyToManyField(Doctor, verbose_name='Врачи')
     status = models.ForeignKey(Status, on_delete=models.SET_DEFAULT,default=1)
 
     def __str__(self):
